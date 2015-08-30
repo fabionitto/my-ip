82c8e8d56b78d6ba72bcb97eebe325e6c5b111ba
rohithpr <praroh2@gmail.com>
2015-08-30 13:49:21 +0530

    Check Heroku

--- server.py
+++ server.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def send_ip():
-    data = {
-        'ip': request.remote_addr,
-    }
-    return jsonify(data)
+    # data = {
+    #     'ip': request.remote_addr,
+    # }
+    # return jsonify(data)
+    return 'foo'
 
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run(threaded=True, host='0.0.0.0', port=80)
+    # app.run(threaded=True, host='0.0.0.0', port=80)
