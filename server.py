dcbcbdb87468a0b0a6c6254af3109b551df1098a
rohithpr <praroh2@gmail.com>
2015-08-30 13:52:02 +0530

    Check Heroku

--- server.py
+++ server.py
@@ -11,5 +11,5 @@ def send_ip():
     return 'foo'
 
 if __name__ == '__main__':
-    app.run(threaded=True, host='0.0.0.0', port=80)
+    app.run()
     # app.run(threaded=True, host='0.0.0.0', port=80)
