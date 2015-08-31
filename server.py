44e66bad46de1b3e1d4ffbb341e3dcb6917e83ab
rohithpr <praroh2@gmail.com>
2015-08-31 17:46:20 +0530

    Fix for requests received through proxies

--- server.py
+++ server.py
@@ -4,10 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_ip():
+    if not request.headers.getlist("X-Forwarded-For"):
+        ip = request.remote_addr
+    else:
+        ip = request.headers.getlist("X-Forwarded-For")[0]
     data = {
-        'ip': request.remote_addr
+        'ip': ip,
     }
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=80, host='0.0.0.0')
